1821c6303f47838e953d3043335e78ef4dc827d0
Simon Jespersen <simjes91@me.com>
2015-12-25 01:42:47 +0100

    fixed link bug

--- app/controllers/projectCtrl.js
+++ app/controllers/projectCtrl.js
@@ -2,7 +2,6 @@ angular.module('webapp')
     .controller('projectCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.getProjects = function () {
             $http.get('./projects.json').then( function( result) {
-                console.log(result);
                 $scope.projects = result.data.projects;
             }); // handle errir
         }
