6e6856b44829fc70db017a44d75c3dadbe074de4
Simon Jespersen <simjes91@me.com>
2015-12-27 01:22:12 +0100

    dynamic columns started

--- app/controllers/projectCtrl.js
+++ app/controllers/projectCtrl.js
@@ -1,8 +1,54 @@
 angular.module('webapp')
-    .controller('projectCtrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('projectCtrl', ['$scope', '$http', '$mdMedia', function ($scope, $http, $mdMedia) {
         $scope.getProjects = function () {
-            $http.get('./projects.json').then( function( result) {
-                $scope.projects = result.data.projects;
-            }); // handle errir
+            $http.get('./projects.json').then(function (result) {
+                $scope.projectColumns = getProjectInfo(result.data.projects);
+                console.log($scope.projectColumns);
+
+            }); // handle error
+        }
+
+        function getProjectInfo(projects) {
+            var columns = findSize();
+            var numberOfCards = projects.length;
+            var cardsPerColumn = findCardsPerColumn(numberOfCards);
+            var cardCounter = 0;
+            var allColumnInfo = [];
+
+            for (var i = 0; i < columns; i++) {
+                var columnCards = {};
+                for (j = 0; j < cardsPerColumn; j++) {
+                    if (cardCounter < numberOfCards) {
+                        columnCards["card" + j] = projects[cardCounter];
+                        cardCounter++;
+                    }   
+                }
+                allColumnInfo.push(columnCards);
+            }
+            return allColumnInfo;
+        }
+
+        function findSize() {
+            if ($mdMedia('xs')) {
+                return 1;
+            } else if ($mdMedia('sm')) {
+                return 2;
+            } else if ($mdMedia('md')) {
+                return 3;
+            } else {
+                return 4;
+            }
+        }
+
+        function findCardsPerColumn(numberOfCards) {
+            if ($mdMedia('xs')) {
+                return numberOfCards;
+            } else if ($mdMedia('sm')) {
+                return Math.ceil(numberOfCards / 2);
+            } else if ($mdMedia('md')) {
+                return Math.ceil(numberOfCards / 3);
+            } else {
+                return Math.ceil(numberOfCards / 4);
+            }
         }
     }]);
