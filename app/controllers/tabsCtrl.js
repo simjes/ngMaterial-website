a1f3a586c6a54e1294209fb01fc368d67626ddde
Simon Jespersen <simjes91@me.com>
2015-12-30 02:11:46 +0100

    new pic

--- app/controllers/tabsCtrl.js
+++ app/controllers/tabsCtrl.js
@@ -10,4 +10,10 @@ angular.module('webapp')
                     break;
             }
         }
+        
+        /*$scope.$watch(function() {
+            return $state.current.name;
+        }, function(newVal, oldVal) {
+            console.log(newVal);
+        });*/
     }]);
