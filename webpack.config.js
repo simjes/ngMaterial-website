c73475e5e5d96fc36a508154e8ae1b53c808c104
Simon Jespersen <s.jespersen@stud.uis.no>
2016-02-06 02:56:24 +0100

    remote testing

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     entry: [
         'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:8080',
+        'webpack-dev-server/client?http://192.168.0.23:8080',
         path.resolve(__dirname, 'app/main.js')
     ],
     output: {
