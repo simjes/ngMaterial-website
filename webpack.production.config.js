497a54f0b45595aa561df5ce92341def01ae75d3
Simon Jespersen <s.jespersen@stud.uis.no>
2016-08-27 10:26:39 +0200

    renamed dist folder to docs for github pages

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, 'app/main.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: "bundle.js"
     },
     module: {
